85f478076aa491cfe2a8573a3a7880f65b6d3018
BILMZA <bilalhamza568055@gmail.com>
2026-05-01 14:50:08 +0500

    fix duplicate fetch error

--- backend/test.js
+++ backend/test.js
@@ -18,24 +18,4 @@ async function testAPI() {
   }
 }
 
-testAPI();const fetch = require("node-fetch");
-
-async function testAPI() {
-  try {
-    const res = await fetch("http://localhost:5000");
-    const text = await res.text();
-
-    if (text.includes("Hello")) {
-      console.log("PASS");
-      process.exit(0);
-    } else {
-      console.log("FAIL");
-      process.exit(1);
-    }
-  } catch (err) {
-    console.log("ERROR", err);
-    process.exit(1);
-  }
-}
-
 testAPI();
